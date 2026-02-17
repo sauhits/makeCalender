a860c951be03157e8be3596562b834f46d3bf9d3
sauhits <atsushi.obata1207@gmail.com>
2026-02-17 21:29:24 +0900

    change / add env TOKENを環境より取得するように変更

--- app/auth.py
+++ app/auth.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import APIKeyHeader
+import os
 
 app = FastAPI()
 
 api_key_header = APIKeyHeader(name="Authorization", auto_error=True)
 
+
 def verify_token(auth_header: str = Depends(api_key_header)):
-    if auth_header != "expected_token":
+    API_TOKEN = os.getenv("TOKEN")
+    if auth_header != API_TOKEN:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Invalid authentication token",
